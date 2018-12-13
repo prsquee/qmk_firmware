2dbc57a69b1705bdc73ef4f090bd04d127d961d3
prsquee <github@fnord.com.ar>
2018-12-13 01:31:44 -0300

    swapped enter with quote

--- keyboards/planck/keymaps/prsquee/keymap.c
+++ keyboards/planck/keymaps/prsquee/keymap.c
@@ -34,7 +34,6 @@ enum planck_keycodes {
   EMOJI,
   CONSOLE,
   S1,
-  S2,
 };
 
 #define LOWER MO(_LOWER)
@@ -47,53 +46,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------------------------------.
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  BS  |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * | HYP  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;: |  '"  |
+   * | HYP  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;: |enter |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * | S/Dot|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /? |s/Entr|
+   * |shift.|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /? |shift'|
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Ctrl |  😃  | Alt  | GUI  |Lower |    Space   |Raise | Left | Down |  Up  |Right |
+   * |Ctrl\ |  😃  | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_grid(
-      KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,            KC_O,    KC_P,    KC_BSPC,
-      MEH_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT,
-      LSFT_T(KC_DOT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH, KC_SFTENT,
-      LCTL_T(KC_PIPE),  EMOJI,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   RALT_T(KC_LEFT), KC_DOWN, KC_UP,   RGUI_T(KC_RGHT)
+      KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,            KC_O,    KC_P,    KC_BSPC,
+      MEH_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,            KC_L,    KC_SCLN, KC_ENT,
+      LSFT_T(KC_COLN),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,         KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT),
+      LCTL_T(KC_PIPE),  EMOJI,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,  RALT_T(KC_LEFT), KC_DOWN, KC_UP,   RGUI_T(KC_RGHT)
   ),
 
   /* Raise
    * ,-----------------------------------------------------------------------------------.
    * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -_ |   =+ | BS   |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  ~   |insert| home | pgup |      |      |      |   (  |   )  |   [{ |   ]} |  |   |
+   * |   ~  | .    |      |      |      |      |      |   (  |   )  |   [{ |   ]} |  |   |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      | delet| end  | pgdn |      |      |      |      |      |      |      |enter |
+   * |      |      |      |      |      | mute | vol- | vol+ |  <<  | play |  >>  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |             |      | mute | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |             |      |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT_planck_grid(
-      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,   KC_MINS,     KC_EQL,    _______,
-      KC_TILD, KC_INS,  KC_HOME, KC_PGUP, _______, _______, _______, KC_LPRN, KC_RPRN,   KC_LBRC,     KC_RBRC,   KC_PIPE,
-      _______, KC_DEL,  KC_END,  KC_PGDN, _______, _______, _______, _______, _______,   _______,     _______,   _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, KC__MUTE,  KC__VOLDOWN, KC__VOLUP, KC_MPLY
+      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,     KC_AMPR,    KC_ASTR, KC_MINS, KC_EQL,  _______,
+      KC_TILD, KC_INS,  _______, _______, _______, _______,  KC__MUTE,    KC_LPRN,    KC_RPRN, KC_LBRC, KC_RBRC, KC_PIPE,
+      _______, _______, _______, _______, _______, KC__MUTE, KC__VOLDOWN, KC__VOLUP,  KC_MRWD, KC_MPLY, KC_MFFD, _______,
+      _______, _______, _______, _______, _______, _______,  _______,     _______,    _______, _______, _______, _______
   ),
 
   /* Lower
    * ,-----------------------------------------------------------------------------------.
    * |   ~  |  0   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |  BS  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Del  | home |  up  | end  |  S1  |      |      |   *  |   4  |   5  |   6  |  +   |
+   * | Del  | home |  up  | end  | pgup |      |      |   *  |   4  |   5  |   6  |  +   |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      | left | down | right|  S2  |      |      |   /  |   1  |   2  |   3  |  -   |
+   * |      | left | down | right| pgdn |      |      |   /  |   1  |   2  |   3  |  -   |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |   ALFRED    |      |   0  |   .  |   =  | enter|
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_grid(
       CONSOLE, KC_0,    KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7, KC_8,  KC_9,     _______,
-      KC_DEL,  KC_HOME, KC_UP,    KC_END,   S1,      _______, _______, KC_PAST,  KC_4, KC_5,  KC_6,     KC_PPLS,
-      _______, KC_LEFT, KC_DOWN,  KC_RIGHT, S2,      _______, _______, KC_PSLS,  KC_1, KC_2,  KC_3,     KC_PMNS,
+      KC_DEL,  KC_HOME, KC_UP,    KC_END,   KC_PGUP, _______, _______, KC_PAST,  KC_4, KC_5,  KC_6,     KC_PPLS,
+      _______, KC_LEFT, KC_DOWN,  KC_RIGHT, KC_PGDN, _______, _______, KC_PSLS,  KC_1, KC_2,  KC_3,     KC_PMNS,
       _______, _______, _______,  _______,  _______, ALFRED,  _______, _______,  KC_0, KC_DOT, KC_EQL,  KC_PENT
   ),
   /* SQUEE
@@ -107,12 +106,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |  /   |   0  |  .   | enter|      |             |      | mute | prev | next | Play |
    * `-----------------------------------------------------------------------------------'
    */
+  /* [_SQUEE] = LAYOUT_planck_grid( */
+  /*     KC_PPLS, KC_P7, KC_P8,  KC_P9,   KC_EQL,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_BSPC, */
+  /*     KC_PMNS, KC_P4, KC_P5,  KC_P6,   _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_QUOT, */
+  /*     KC_PAST, KC_P1, KC_P2,  KC_P3,   _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_ENT , */
+  /*     KC_PSLS, KC_P0, KC_DOT, KC_PENT, LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT */
+  /* ), */
   [_SQUEE] = LAYOUT_planck_grid(
-      KC_PPLS, KC_P7, KC_P8,  KC_P9,   KC_EQL,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_BSPC,
-      KC_PMNS, KC_P4, KC_P5,  KC_P6,   _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
-      KC_PAST, KC_P1, KC_P2,  KC_P3,   _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_ENT ,
-      KC_PSLS, KC_P0, KC_DOT, KC_PENT, LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT
-  ),
+      KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,   KC_I,            KC_O,    KC_P,    KC_BSPC,
+      MEH_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,   KC_K,            KC_L,    KC_SCLN, KC_ENT,
+      LSFT_T(KC_DOT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,   KC_COMM,         KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT),
+      LCTL_T(KC_PIPE),  EMOJI,   KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_SPC,  RAISE,  RALT_T(KC_LEFT), KC_DOWN, KC_UP,   RGUI_T(KC_RGHT)
+      ),
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------------------------------------------------.
@@ -127,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] = LAYOUT_planck_grid(
     KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,
-    RESET,    DEBUG,  _______,  _______, _______, QWERTY,   SQUEE,   _______, _______,  _______,  _______,  _______,
+    RESET,    DEBUG,  _______,  _______,  S1,       QWERTY,   SQUEE,   _______, _______,  _______,  _______,  _______,
     CK_ON,    CK_OFF,   CK_UP,  CK_DOWN,  CK_RST,   MU_ON,    MU_OFF,  _______, _______,  _______,  _______,  _______,
     _______,  _______,  _______, _______, _______,  _______,  _______, _______, _______,  _______,  _______,  _______
   )
@@ -135,58 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 #ifdef AUDIO_ENABLE
-  float test1[][2] = SONG(      H__NOTE(_G5),  \
-                                H__NOTE(_FS5), \
-                                H__NOTE(_E5),  \
-                                H__NOTE(_E5),  \
-                                W__NOTE(_FS5)  );
-
-  float test2[][2] = SONG (
-                                H__NOTE(_A4), \
-                                H__NOTE(_AS4), \
-                                W__NOTE(_C5), \
-                                W__NOTE(_F5), \
-                                H__NOTE(_E5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_C5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_C5), \
-                                W__NOTE(_C5), \
-                                W__NOTE(_C5), \
-                                H__NOTE(_A4), \
-                                H__NOTE(_AS4), \
-                                W__NOTE(_C5), \
-                                W__NOTE(_F5), \
-                                H__NOTE(_G5), \
-                                H__NOTE(_F5), \
-                                H__NOTE(_E5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_E5), \
-                                W__NOTE(_F5), \
-                                W__NOTE(_F5), \
-                                H__NOTE(_G5), \
-                                H__NOTE(_A5), \
-                                H__NOTE(_AS5), \
-                                H__NOTE(_AS5), \
-                                W__NOTE(_A5), \
-                                W__NOTE(_G5), \
-                                H__NOTE(_F5), \
-                                H__NOTE(_G5), \
-                                H__NOTE(_A5), \
-                                H__NOTE(_A5), \
-                                W__NOTE(_G5), \
-                                W__NOTE(_F5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_C5), \
-                                H__NOTE(_D5), \
-                                H__NOTE(_F5), \
-                                H__NOTE(_F5), \
-                                W__NOTE(_E5), \
-                                H__NOTE(_E5), \
-                                H__NOTE(_FS5), \
-                                H__NOTE(_FS5) );
+  float stillalive[][2] = SONG(STILL_ALIVE);
 #endif
 
 uint32_t layer_state_set_user(uint32_t state) {
@@ -229,13 +183,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case S1:
       if (record->event.pressed) {
-        PLAY_SONG(test1);
-      }
-      return false;
-      break;
-    case S2:
-      if (record->event.pressed) {
-        PLAY_SONG(test2);
+        PLAY_SONG(stillalive);
       }
       return false;
       break;
